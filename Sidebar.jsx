6e4570950a6ce4cadcacc36bd8c767360694feb6
muhmmad Nabh <muhammadnabih2020@gmail.com>
2024-05-10 03:59:09 +0300

    Fix Side bar

--- Sidebar.jsx
+++ Sidebar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div>
+      <h1>Sidebar</h1>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
